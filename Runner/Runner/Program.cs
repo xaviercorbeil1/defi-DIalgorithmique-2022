16792c68943d6a9275c8ad341374c91e11c4a4af
Xavier Corbeil <xcorbeil@dialoginsight.com>
2022-06-15 16:03:23 -0400

    basicCalculator

--- Runner/Runner/Program.cs
+++ Runner/Runner/Program.cs
@@ -1,19 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 var solution = new Solution();
 
-var sol1 = solution.ValidPalindrome("abca");
-var sol2 = solution.ValidPalindrome("aba");
 
+Console.WriteLine(solution.Calculate("2+2"));
+Console.WriteLine(solution.Calculate("3+2*2"));
+Console.WriteLine(solution.Calculate(" 3/2 "));
+Console.WriteLine(solution.Calculate(" 3+5 / 2 "));
 
-Console.WriteLine(sol1);
-Console.WriteLine(sol2);
-Console.WriteLine(solution.ValidPalindrome("racecar"));
-Console.WriteLine(solution.ValidPalindrome("aracecar"));
-Console.WriteLine(solution.ValidPalindrome("raacecar"));
-Console.WriteLine(solution.ValidPalindrome("racecar"));
-Console.WriteLine(solution.ValidPalindrome("raceecar"));
-Console.WriteLine(solution.ValidPalindrome("racecarr"));
-Console.WriteLine(solution.ValidPalindrome("racecaru"));
-Console.WriteLine(solution.ValidPalindrome("aracecare"));
 
 
