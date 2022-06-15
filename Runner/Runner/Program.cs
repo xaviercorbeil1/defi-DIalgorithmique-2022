b718a688f5f69af4718098af5efe515cc4b87e2b
Xavier Corbeil <xcorbeil@dialoginsight.com>
2022-06-15 14:21:08 -0400

    missingRanges

---
+++ Runner/Runner/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+var solution = new Solution();
+
+var sol1 = String.Join(',', solution.FindMissingRanges(new[] { 0, 1, 3, 50, 75 }, 0, 99));
+var sol2 = String.Join(',', solution.FindMissingRanges(new[] { -1 }, -1, -1));
+
+Console.WriteLine(sol1);
+Console.WriteLine(sol2);
+
